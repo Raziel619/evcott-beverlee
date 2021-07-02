516d01483ddda939ec079c8df56e9b8c262b564f
platinumcrushedice <clarenceramai@gmail.com>
2021-07-02 11:06:06 -0400

    Added Page Refresh to Live Data Page

--- resources/views/live/live.blade.php
+++ resources/views/live/live.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app', ['class' => 'bg-default'])
-
+<meta http-equiv="refresh" content="10">
 @section('content')
 <div class="header bg-gradient-primary py-7 py-lg-8">
     <div class="container">
