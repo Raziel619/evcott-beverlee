7805b54f301f6c3283952d42f956d775be5138e6
Shankar Ramharack <shankar.ramharack@gmail.com>
2022-08-15 17:54:28 -0400

    Revert "Added materials from publication"
    This reverts commit 01f482f4e301035405fd0a550befa9ab5c7275d8.
    
    Updated changes since paper submission
    
    Finalized dash as presented in paper
    
    Added materials from publication
    
    Reorganized data
    
    Data available upon request

---
+++ data_analysis/pages/utility_reporting.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+import altair as alt
+from vega_datasets import data
+
+st.header("Utility Reporting")
+
+st.write("This page contains a visualization of the charging and discharging curves of the specimen\n"+
+            "A mathematical model is used to fit the data, and its metrics and results are summarized.")
